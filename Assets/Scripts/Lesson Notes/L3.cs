9ffd741c9ff9df65c0c008b55f4f763ccd9e9e2e
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-20 18:20:42 -0500

    Lesson 3: Added run animation and transitions to and from idle

---
+++ Assets/Scripts/Lesson Notes/L3.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L3 : MonoBehaviour
+{
+    /*
+     animation
+        when you have an animation with keyframes, there are blue tabs on each end of a line of keyframes you can use to make animations slower or faster over time
+        
+    
+     */
+}
