5ff422a7321c3e56e6e30f669ec779086882e363
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-20 17:41:54 -0500

    Lesson 2: have CharacterGrounding and PlayerMovementController linked together

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -8,27 +8,34 @@ public class CharacterGrounding : MonoBehaviour
     [SerializeField] Transform _rightFoot;
     [SerializeField] float _maxDistance;
     [SerializeField] LayerMask _layerMask;
-    [SerializeField] bool isGrounded;
+    
+    //other classes can get the value, but not set it
+    public bool IsGrounded { get; private set; }
 
     void Update()
     {
         CheckFoodForGrounding(_leftFoot);
-        if(isGrounded == false)
+        if(IsGrounded == false)
             CheckFoodForGrounding(_rightFoot);
 
     }
 
     void CheckFoodForGrounding(Transform foot)
     {
+        //ray cast hit on a collider
         var rayCastHit = Physics2D.Raycast(foot.position, Vector2.down, _maxDistance, _layerMask);
+        
+        //debug ray that can be seen in the scene view
         Debug.DrawRay(foot.position, Vector3.down * _maxDistance, Color.red);
+        
+        //if the ray cast hits a collider, especially the collider of the ground, the player is on the ground
         if (rayCastHit.collider != null)
         {
-            isGrounded = true;
+            IsGrounded = true;
         }
         else
         {
-            isGrounded = false;
+            IsGrounded = false;
         }
     }
 }
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -3,16 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//these components depend on each other so when one is added to an object, the other is added as well 
+[RequireComponent(typeof(CharacterGrounding))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 2;
     [SerializeField] float jumpForce = 400;
     Rigidbody2D _rigidBody2D;
+    CharacterGrounding _characterGrounding;
 
     void Awake()
     {
         //rigidbody2D reference
         _rigidBody2D = GetComponent<Rigidbody2D>();
+        
+        //characterGrounding script reference
+        _characterGrounding = GetComponent<CharacterGrounding>();
     }
 
     void FixedUpdate()
@@ -28,7 +35,7 @@ public class PlayerMovementController : MonoBehaviour
         transform.position += movement * (Time.deltaTime * moveSpeed);
 
         //if Fire1 is pressed this frame
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && _characterGrounding.IsGrounded)
         {
             //force added to the rigidbody in the up direction multiplied by a jump force that can be edited in the inspector
             _rigidBody2D.AddForce(Vector2.up * jumpForce);
