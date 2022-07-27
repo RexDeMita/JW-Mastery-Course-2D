a35f4ca37cea3cad01a44f786087c93d8ad1a04e
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-27 18:55:01 -0500

    Lesson 8: kept the player from moving when on a platform

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -8,6 +8,11 @@ public class CharacterGrounding : MonoBehaviour
     [SerializeField] Transform _rightFoot;
     [SerializeField] float _maxDistance;
     [SerializeField] LayerMask _layerMask;
+
+    Transform groundedObject;
+
+    //this vector3 could be null. that is what the ? is checking for
+    Vector3? groundedObjectLastPosition; 
     
     //other classes can get the value, but not set it
     public bool IsGrounded { get; private set; }
@@ -18,6 +23,30 @@ public class CharacterGrounding : MonoBehaviour
         if(IsGrounded == false)
             CheckFoodForGrounding(_rightFoot);
 
+        StickToMovingObjects();
+    }
+
+    void StickToMovingObjects()
+    {
+        //is the player grounded 
+        if (groundedObject != null)
+        {
+            //if the grounded object's last position exists and the current and last positions of the object do not equal each other
+            if (groundedObjectLastPosition.HasValue && groundedObjectLastPosition.Value != groundedObject.position)
+            {
+                //figure out the different between the current position of the grounded object and the last position of the grounded object
+                Vector3 delta = groundedObject.position - groundedObjectLastPosition.Value; 
+                
+                //this sets the player's position in relation to the object moving
+                transform.position += delta;
+            }
+            //this sets the last position of the grounding object to the current position of the grounded object 
+            groundedObjectLastPosition = groundedObject.position;
+        }
+        else
+        {
+            groundedObjectLastPosition = null; 
+        }
     }
 
     void CheckFoodForGrounding(Transform foot)
@@ -32,9 +61,15 @@ public class CharacterGrounding : MonoBehaviour
         if (rayCastHit.collider != null)
         {
             IsGrounded = true;
+            //get the transform of the collider that this raycast hit
+            groundedObject = rayCastHit.collider.transform; 
+            
+            
         }
         else
         {
+            //there is no grounded object anymore
+            groundedObject = null;
             IsGrounded = false;
         }
     }
